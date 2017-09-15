5cbb2056eac8236c491d2803af0a005b0be87398
Rob Gwynn-Jones <gwynnr@gmail.com>
2017-09-15 16:49:03 +0100

    Add code comments, and store descendent method list on URL path nodes

--- project/urlpathtree.py
+++ project/urlpathtree.py
@@ -6,16 +6,27 @@ REST_METHODS = ['get', 'post', 'put', 'delete']
 
 class urlpathnode(object):
     def __init__(self, name, is_full_url=False):
+        # the URL segment that this node represents
         self.name = name
+        # does the path from the root node to this node represent a complete URL?
         self.is_full_url = is_full_url
+        # what REST methods are available on this node?
         self.methods = []
+        # what REST methods are available on this node or children of this node?
+        self.descendent_methods = []
+        # dictionary of child nodes, with node names as keys
         self.children = {}
 
     def __eq__(self, other_name):
         return other_name == self.name
 
     def print_node(self, accumulator=""):
-        s = "{}{}{} ({})".format(accumulator, self.name, " (*)" if self.is_full_url else "", self.methods)
+        s = "{}{}{} ({}) ({})".format(
+            accumulator,
+            self.name,
+            " (*)" if self.is_full_url else "",
+            self.methods,
+            self.descendent_methods)
         accumulator += "    "
         for child in self.children.values():
             s += "\n{}".format(child.print_node(accumulator))
@@ -27,18 +38,31 @@ class urlpathnode(object):
 
     def add_child(self, child_url_nodes, methods):
         first, rest = child_url_nodes[0], child_url_nodes[1:]
+
+        # some descendent of this node will have all members of `methods`
+        # available, so add all members of `methods` to `descendent_methods`
+        self.descendent_methods.extend(methods)
+        self.descendent_methods = list(set(self.descendent_methods))
+
+        # find the appropriate child node based on the first URL segment.  If
+        # no such child exists, create one
         try:
             child = self.children[first]
         except KeyError as e:
             child = urlpathnode(first)
             self.children[first] = child
 
+        # if there are more child nodes to process, do so recursively
         if rest:
             child.add_child(rest, methods)
+        # if we are on the last child node, add the provided `methods` to this
+        # node's `methods` list, as well as `descendent_methods` list
         else:
             child.is_full_url = True
             child.methods.extend(methods)
             child.methods = list(set(child.methods))
+            child.descendent_methods.extend(methods)
+            child.descendent_methods = list(set(child.descendent_methods))
 
     def get_child_names(self):
         return sorted([child.name for child in self.children])
